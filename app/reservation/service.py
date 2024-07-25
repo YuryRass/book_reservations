fbc0cae54257061cbba9b902e7103c89e1d17f80
YuryRass <yurchik1997yurchik@gmail.com>
2024-07-26 01:41:40 +0300

    modified

--- app/reservation/service.py
+++ app/reservation/service.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from celery.result import AsyncResult
 
+from app.logger import logger
 from app.reservation.async_redis import RedisClient
 from app.reservation.dao import ReservationDAO
 from app.reservation.shemas import ReservationCreate, ReservationResponse
@@ -14,33 +15,51 @@ class ReservationService:
     @classmethod
     async def create_reservation(cls, reservation: ReservationCreate):
         """Бронирование книги."""
-        new_reservation = await ReservationDAO.reserve_book(reservation)
+        try:
+            new_reservation = await ReservationDAO.reserve_book(reservation)
 
-        #  Удаление брони в фоновом режиме
-        diff_datetime = reservation.end_date - reservation.start_date
-        result = delete_reservation.apply_async(
-            args=[new_reservation.id],
-            eta=datetime.now() + diff_datetime,
-        )
+            logger.info(f"Бронь номер {new_reservation.id} создана")
+            #  Период. задача удаления брони в фоновом режиме
+            diff_datetime = reservation.end_date - reservation.start_date
+            eta_time = datetime.now() + diff_datetime
+            result = delete_reservation.apply_async(
+                args=[new_reservation.id],
+                eta=eta_time,
+            )
+            logger.info(
+                f"Отмена брони номер {new_reservation.id} назначена на "
+                f"{eta_time.strftime('%Y-%m-%d %H:%M:%S')}"
+            )
 
-        # Добавляем в redis в качестве ключа - ID бронирования,
-        # в качестве значения - ID запущенной задачи
+            # Добавляем в redis в качестве ключа - ID бронирования,
+            # в качестве значения - ID запущенной задачи
+            await RedisClient.set(new_reservation.id, result.id)
 
-        await RedisClient.set(new_reservation.id, result.id)
-        print("Result ID: ", result.id, type(result.id))
-
-        return new_reservation
+            return new_reservation
+        except Exception as ex:
+            logger.error(
+                f'Ошибка в бронировании книги: {ex}', exc_info=True,
+            )
+            raise
 
     @classmethod
     async def return_book(cls, reservation_id: int) -> ReservationResponse:
         """Отмена брони."""
         reservation = await ReservationDAO.return_book(reservation_id)
 
-        # отменяем выполнение фоновой задачи
-        task_id = await RedisClient.pop(reservation_id)
+        try:
+            logger.info("Отмена выполнения фоновой задачи...")
+            task_id = await RedisClient.pop(reservation_id)
 
-        assert task_id is not None
-        async_res = AsyncResult(str(task_id))
-        async_res.revoke(terminate=async_res.ready())
+            assert task_id is not None
+            async_res = AsyncResult(str(task_id))
+            async_res.revoke(terminate=async_res.ready())
 
-        return reservation
+            logger.info(f"Задача номер {task_id.decode('utf-8')} отменена")
+            return reservation
+        except Exception as ex:
+            logger.error(
+                f'Ошибка в отмене задачи номер {task_id}: {ex}',
+                exc_info=True,
+            )
+            raise
